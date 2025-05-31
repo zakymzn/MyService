2e66477904cba7aa1d53767cfe1057553b2d73f9
zakymzn <mamurzakynurrokhman@gmail.com>
2025-06-01 00:22:17 +0700

    latihan bound service

--- app/src/main/java/com/example/myservice/MainActivity.kt
+++ app/src/main/java/com/example/myservice/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.myservice
 
 import android.Manifest
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
@@ -23,6 +26,28 @@ class MainActivity : AppCompatActivity() {
     ) { isGranted: Boolean? ->
         if (!isGranted!!) {
             Toast.makeText(this, "Unable to display Foreground service notification due to permission decline", Toast.LENGTH_LONG)
+        }
+    }
+
+    private var boundStatus = false
+    private lateinit var boundService: MyBoundService
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+            boundStatus = false
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val myBinder = service as MyBoundService.MyBinder
+            boundService = myBinder.getService
+            boundStatus = true
+            getNumberFromService()
+        }
+    }
+
+    private fun getNumberFromService() {
+        boundService.numberLiveData.observe(this) { number ->
+            binding.tvBoundServiceNumber.text = number.toString()
         }
     }
 
@@ -54,6 +79,22 @@ class MainActivity : AppCompatActivity() {
         }
         binding.btnStopForegroundService.setOnClickListener {
             stopService(foregroundServiceIntent)
+        }
+
+        val boundServiceIntent = Intent(this, MyBoundService::class.java)
+        binding.btnStartBoundService.setOnClickListener {
+            bindService(boundServiceIntent, connection, BIND_AUTO_CREATE)
+        }
+        binding.btnStopBoundService.setOnClickListener {
+            unbindService(connection)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (boundStatus) {
+            unbindService(connection)
+            boundStatus = false
         }
     }
 }
